8ee6056a3ff063edf8add2007efc863abeff94a9
leidy <leidyjohanasantacruzdaza03@gmail.com>
2023-09-19 17:03:57 -0500

    Initial commit barra de navegacion

---
+++ app/src/main/java/com/example/barradenavegacion/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.ayush.popupmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.barradenavegacion.HomeFragment
+import com.example.barradenavegacion.MenuFragment
+import com.example.barradenavegacion.R
+import com.example.barradenavegacion.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.message -> {
+                    loadFragment(MenuFragment())
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(SettingsFragment())
+                    true
+                }
+
+                else -> {
+                    true
+                }
+            }
+        }
+    }
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
+}
